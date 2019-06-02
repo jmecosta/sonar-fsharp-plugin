247512c23420742107b8c87033b7406e18f5d2a7
Volker Milbrandt <milbrandt@gmx.de>
2019-06-02 10:19:53 +0200

    update patch number (same public API, updated to new appyoyer-image)

--- FsSonarRunner/FsSonarRunner/AssemblyInfo.fs
+++ FsSonarRunner/FsSonarRunner/AssemblyInfo.fs
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("FsSonarRunner")>]
-[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015, 2019")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
@@ -33,9 +33,8 @@ open System.Runtime.InteropServices
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.3.0")>]
-[<assembly: AssemblyFileVersion("1.0.3.0")>]
+[<assembly: AssemblyVersion("1.0.4.0")>]
+[<assembly: AssemblyFileVersion("1.0.4.0")>]
 
 do
     ()
