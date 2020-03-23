f33af338cf79967fda90c29284cfd0f8d69ab0e5
Volker Milbrandt <milbrandt@gmx.de>
2020-03-23 22:09:48 +0100

    add tests for ConfHelper.GetValueForEnum

--- FsSonarRunner/FsSonarRunnerCore.Test/ConfHelperTests.fs
+++ FsSonarRunner/FsSonarRunnerCore.Test/ConfHelperTests.fs
@@ -3,15 +3,52 @@
 open NUnit.Framework
 open FsSonarRunnerCore
 
+type TestEnum = 
+    | TestValue0 = 0
+    | TestValue1 = 1
+    | TestValue2 = 2
+    | TestValue3 = 3
+
 // TODO
 [<TestFixture>]
 type ConfHelperTests() =
+    let configValue = TestEnum.TestValue1
+    let config = 
+        sprintf "<AnalysisInput>
+            <Rules>
+                <Rule>
+                    <Key>EnumTestRule</Key>
+                    <Parameters>
+                        <Parameter>
+                            <Key>EnumTestParam</Key>
+                            <Value>%s</Value>
+                        </Parameter>
+                    </Parameters>
+                </Rule>
+                <Rule>
+                    <Key>RuleWithoutParameter</Key>
+                </Rule>
+            </Rules>
+        </AnalysisInput>" (configValue.ToString())
+        |> ConfHelper.InputConfigution.Parse
+    let defaultValue = TestEnum.TestValue2
 
-    [<Test>][<Ignore("not implemented yet")>]
-    member this.GetValueForEnum_defaultValue() =
-        //ConfHelper.GetValueForEnum()
-        Assert.Fail()
+    [<Test>]
+    member this.GetValueForEnum_NoRule_defaultValue() =
+        let actual: TestEnum = ConfHelper.GetValueForEnum(config, "NoEnumTestRule", "EnumTestParam",  defaultValue.ToString())
+        Assert.AreEqual(defaultValue, actual)
 
-    [<Test>][<Ignore("not implemented yet")>]
-    member this.GetValueForEnum_parsedValue() =
-        Assert.Fail()
+    [<Test>]
+    member this.GetValueForEnum_NoParameter_defaultValue() =
+        let actual: TestEnum = ConfHelper.GetValueForEnum(config, "EnumTestRule", "NoEnumTestParam",  defaultValue.ToString())
+        Assert.AreEqual(defaultValue, actual)
+
+    [<Test>]
+    member this.GetValueForEnum_WithoutParameter_defaultValue() =
+        let actual: TestEnum = ConfHelper.GetValueForEnum(config, "RuleWithoutParameter", "NoEnumTestParam",  defaultValue.ToString())
+        Assert.AreEqual(defaultValue, actual)
+
+    [<Test>]
+    member this.GetValueForEnum_RuleAndParameter_parsedValue() =
+        let actual: TestEnum = ConfHelper.GetValueForEnum(config, "EnumTestRule", "EnumTestParam",  defaultValue.ToString())
+        Assert.AreEqual(configValue, actual)
