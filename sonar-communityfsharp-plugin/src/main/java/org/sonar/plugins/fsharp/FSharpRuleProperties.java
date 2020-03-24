2ea8ba0b0067c8f36f682fd26be027f82859144e
Volker Milbrandt <milbrandt@gmx.de>
2020-03-24 15:54:56 +0100

    reorder rules according to FSharpLint rule numbers

--- sonar-communityfsharp-plugin/src/main/java/org/sonar/plugins/fsharp/FSharpRuleProperties.java
+++ sonar-communityfsharp-plugin/src/main/java/org/sonar/plugins/fsharp/FSharpRuleProperties.java
@@ -31,53 +31,82 @@ final class FSharpRuleProperties {
     static {
         Map<String, RuleProperty> rules = new HashMap<>();
 
-        // Typography
-        rules.put("RulesTypographyTrailingLineError", new RuleProperty(Severity.MINOR, "File should not have a trailing new line", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesTypographyTabCharacterError", new RuleProperty(Severity.MAJOR, "Tabulation character should not be used", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesTypographyFileLengthError", new RuleProperty(Severity.MAJOR, "File should not have too many lines", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesTypographyLineLengthError", new RuleProperty(Severity.MAJOR, "Lines should not be too long", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesTypographyTrailingWhitespaceError", new RuleProperty(Severity.MINOR, "Lines should not have trailing whitespace", EMPTY_HTML_DESCRIPTION));
+        // FL0001-FL0012 Formatting
 
-        // nested statements
-        rules.put("RulesNestedStatementsError", new RuleProperty(Severity.MAJOR, "Maximum allowed of nesting", EMPTY_HTML_DESCRIPTION));
+        // FL0013-FL0058 Conventions
+        rules.put("RulesRedundantNewKeyword", // FL0014
+                new RuleProperty(Severity.MINOR, "Redundant keyword \"new\" should be avoided",
+                        EMPTY_HTML_DESCRIPTION));
 
-        // hint matcher - todo Map of list
+        rules.put("RulesNestedStatementsError", // FL0015
+                new RuleProperty(Severity.MAJOR, "Maximum allowed of nesting", EMPTY_HTML_DESCRIPTION));
+
+        rules.put("RulesFailwithWithSingleArgument", // FL0016
+                new RuleProperty(Severity.MAJOR, "Fail with should have a sigle argument", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesRaiseWithSingleArgument", // FL0017
+                new RuleProperty(Severity.MAJOR, "Expected raise to have a single argument", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesNullArgWithSingleArgument", // FL0018
+                new RuleProperty(Severity.MAJOR, "Expected nullArg to have a single argument", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesInvalidOpWithSingleArgument", // FL0019
+                new RuleProperty(Severity.MAJOR, "Expected invalidOp to have a single argument",
+                        EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesInvalidArgWithTwoArguments", // FL0020
+                new RuleProperty(Severity.MAJOR, "Expected invalidArg to have two arguments", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesFailwithfWithArgumentsMatchingFormatString", // FL0021
+                new RuleProperty(Severity.MAJOR,
+                        "Expected failwithf's arguments to match the format string (there were too many arguments)",
+                        EMPTY_HTML_DESCRIPTION));
+
+        rules.put("RulesSourceLengthError", // FL0022-FL0033
+                new RuleProperty(Severity.MAJOR, "Source length check", EMPTY_HTML_DESCRIPTION));
+
+        rules.put("RulesReimplementsFunction", // FL0034
+                new RuleProperty(Severity.MAJOR, "Pointless function redefines", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesCanBeReplacedWithComposition", // FL0035
+                new RuleProperty(Severity.MAJOR, "Function composition should be used instead of current function",
+                        EMPTY_HTML_DESCRIPTION));
+
+        rules.put("RulesNumberOfItemsTupleError", // FL0051
+                new RuleProperty(Severity.MAJOR, "The maximum number of tuples allowed", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesNumberOfItemsFunctionError", // FL0052
+                new RuleProperty(Severity.MAJOR, "The maximum number of parameters allowed", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesNumberOfItemsClassMembersError", // FL0053
+                new RuleProperty(Severity.MAJOR, "The maximum number of members in class allowed",
+                        EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesNumberOfItemsBooleanConditionsError", // FL0054
+                new RuleProperty(Severity.MAJOR, "Maximum allowed boolean operatores in condition",
+                        EMPTY_HTML_DESCRIPTION));
+
+        rules.put("RulesFavourIgnoreOverLetWildError", // FL0055
+                new RuleProperty(Severity.MAJOR, "Favour using the ignore function rather than let",
+                        EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesWildcardNamedWithAsPattern", // FK0056
+                new RuleProperty(Severity.MAJOR, "Unnecessary wildcard named using the as pattern",
+                        EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesUselessBindingError", // FL0057
+                new RuleProperty(Severity.MAJOR, "Useless binding", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesTupleOfWildcardsError", // FL0058
+                new RuleProperty(Severity.MAJOR,
+                        "A constructor argument in a pattern that is a tuple consisting of entirely wildcards can be replaced with a single wildcard",
+                        EMPTY_HTML_DESCRIPTION));
+
+        // FL0059-FL0064 Typography
+        rules.put("RulesTypographyLineLengthError", // FL0060
+                new RuleProperty(Severity.MAJOR, "Lines should not be too long", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesTypographyTrailingWhitespaceError", // FL0061
+                new RuleProperty(Severity.MINOR, "Lines should not have trailing whitespace", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesTypographyFileLengthError", // FL0062
+                new RuleProperty(Severity.MAJOR, "File should not have too many lines", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesTypographyTrailingLineError", // FL0063
+                new RuleProperty(Severity.MINOR, "File should not have a trailing new line", EMPTY_HTML_DESCRIPTION));
+        rules.put("RulesTypographyTabCharacterError", // FL0064
+                new RuleProperty(Severity.MAJOR, "Tabulation character should not be used", EMPTY_HTML_DESCRIPTION));
+
+        // FL0065 Hints
         rules.put("RulesHintRefactor", new RuleProperty(Severity.INFO, "Hint Refactor", EMPTY_HTML_DESCRIPTION));
-        // rules.put("RulesHintSuggestion", new RuleProperty(Severity.INFO, "Hint Suggestion", EMPTY_HTML_DESCRIPTION));
-
-        // name convention
-        // rules.put("RulesNamingConventionsCamelCaseError", new RuleProperty(Severity.INFO, "Use PascalCasing for all public member, type, and namespace names consisting of multiple words", EMPTY_HTML_DESCRIPTION));
-        // rules.put("RulesNamingConventionsPascalCaseError", new RuleProperty(Severity.INFO, "Use camelCasing for parameter names", EMPTY_HTML_DESCRIPTION));
-
-        // RaiseWithTooManyArguments
-        rules.put("RulesRaiseWithSingleArgument", new RuleProperty(Severity.MAJOR, "Expected raise to have a single argument", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesFailwithWithSingleArgument", new RuleProperty(Severity.MAJOR, "Fail with should have a sigle argument", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesNullArgWithSingleArgument", new RuleProperty(Severity.MAJOR, "Expected nullArg to have a single argument", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesInvalidOpWithSingleArgument", new RuleProperty(Severity.MAJOR, "Expected invalidOp to have a single argument", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesInvalidArgWithTwoArguments", new RuleProperty(Severity.MAJOR, "Expected invalidArg to have two arguments", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesFailwithfWithArgumentsMatchingFormatString", new RuleProperty(Severity.MAJOR, "Expected failwithf's arguments to match the format string (there were too many arguments)", EMPTY_HTML_DESCRIPTION));
-
-        // bindings
-        rules.put("RulesTupleOfWildcardsError", new RuleProperty(Severity.MAJOR, "A constructor argument in a pattern that is a tuple consisting of entirely wildcards can be replaced with a single wildcard", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesWildcardNamedWithAsPattern", new RuleProperty(Severity.MAJOR, "Unnecessary wildcard named using the as pattern", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesUselessBindingError", new RuleProperty(Severity.MAJOR, "Useless binding", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesFavourIgnoreOverLetWildError", new RuleProperty(Severity.MAJOR, "Favour using the ignore function rather than let", EMPTY_HTML_DESCRIPTION));
-
-        // function reinplementation
-        rules.put("RulesCanBeReplacedWithComposition", new RuleProperty(Severity.MAJOR, "Function composition should be used instead of current function", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesReimplementsFunction", new RuleProperty(Severity.MAJOR, "Pointless function redefines", EMPTY_HTML_DESCRIPTION));
-
-        // source length
-        rules.put("RulesSourceLengthError", new RuleProperty(Severity.MAJOR, "Source length check", EMPTY_HTML_DESCRIPTION));
-
-        // NumberOfItems
-        rules.put("RulesNumberOfItemsTupleError", new RuleProperty(Severity.MAJOR, "The maximum number of tuples allowed", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesNumberOfItemsClassMembersError", new RuleProperty(Severity.MAJOR, "The maximum number of members in class allowed", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesNumberOfItemsFunctionError", new RuleProperty(Severity.MAJOR, "The maximum number of parameters allowed", EMPTY_HTML_DESCRIPTION));
-        rules.put("RulesNumberOfItemsBooleanConditionsError", new RuleProperty(Severity.MAJOR, "Maximum allowed boolean operatores in condition", EMPTY_HTML_DESCRIPTION));
-
-        // RulesRedundantNewKeyword
-        rules.put("RulesRedundantNewKeyword", new RuleProperty(Severity.MINOR, "Redundant keyword \"new\" should be avoided", EMPTY_HTML_DESCRIPTION));
+        // TODO hint matcher - Map of list
+        // rules.put("RulesHintSuggestion", new RuleProperty(Severity.INFO, "Hint
+        // Suggestion", EMPTY_HTML_DESCRIPTION));
 
         ALL = unmodifiableMap(rules);
     }
